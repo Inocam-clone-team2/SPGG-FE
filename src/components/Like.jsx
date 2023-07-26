ab70589a31b4c7353d03ea57ecc12111f26e233e
HyerimNac:/Users/나혜림/OneDrive/바탕 화면/test/venv/Scripts/python.exe <rimhyena@gmail.com>
2023-07-26 15:50:12 +0900

    "like 위치 componenets폴더"

---
+++ src/components/Like.jsx
@@ -0,0 +1,64 @@
+import { BsCaretUpFill, BsCaretUp } from "react-icons/bs";
+import React, { useState, useEffect } from "react";
+import { Tooltip } from "antd";
+import instance from "../api/post";
+
+const Like = ({ postId }) => {
+  const [Likes, setLikes] = useState(0);
+  const [LikeAction, setLikeAction] = useState(false);
+  console.log(1);
+  const fetchPostData = async () => {
+    try {
+      const response = await instance.get(`/api/post/${postId}`);
+      const result = response.data.data.isLike;
+      const LikeCount = response.data.data.liked;
+      console.log("res", result);
+
+      if (result) {
+        setLikeAction(true);
+        setLikes(LikeCount);
+      } else {
+        setLikeAction(false);
+        setLikes(LikeCount);
+      }
+    } catch (error) {
+      console.log("API 요청 오류:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPostData();
+  }, []);
+
+  const onToggleLike = async () => {
+    try {
+      const response = await instance.post(`/api/post/${postId}/like`);
+      console.log("response", response);
+      if (response.status === 200) {
+        fetchPostData();
+        setLikes((prevLikes) => (LikeAction ? prevLikes - 1 : prevLikes + 1));
+      } else {
+        console.error("좋아요 요청이 실패하였습니다.");
+      }
+    } catch (error) {
+      alert("로그인을 해주세요");
+      console.error("좋아요 요청 중 오류가 발생했습니다.", error);
+    }
+  };
+  return (
+    <div>
+      <span key="comment-basic-like">
+        <Tooltip title="Like">
+          {LikeAction ? (
+            <BsCaretUpFill onClick={onToggleLike} />
+          ) : (
+            <BsCaretUp onClick={onToggleLike} />
+          )}
+        </Tooltip>
+        <span style={{ paddingLeft: "4px", cursor: "auto" }}> {Likes}</span>
+      </span>
+    </div>
+  );
+};
+
+export default Like;
