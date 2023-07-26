e5dcab75a09f8b96dfa5d023074c683f32634154
solightnsalt <>
2023-07-26 16:24:47 +0900

    Feat: 전적 완료

--- src/components/history/userInfo/UserInfo.jsx
+++ src/components/history/userInfo/UserInfo.jsx
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 
 const UserInfo = (data) => {
 	const playerInfo = data.data.summoner;
+
+	// 새로고침 1분 뒤
+	// const { refreshTime, setRefreshTime } = useState(null);
+	// const handleRefreshTime = () => {
+	// 	const currentTime = new Date();
+	// 	setButtonClickTime(currentTime);
+	// 	setInButtonDisabled(true);
+	// };
 
 	//프로필 아이콘
 	const profileIcon =
@@ -22,7 +30,7 @@ const UserInfo = (data) => {
 					{/* <S.InfoSpan>
 						랭킹 <S.InfoSpan color="blue">3</S.InfoSpan>위(상위 0.0004%)
 					</S.InfoSpan> */}
-					<S.RefreshBtn>전적 갱신</S.RefreshBtn>
+					<S.RefreshBtn onClick={() => location.reload()}>전적 갱신</S.RefreshBtn>
 					{/* <S.InfoSpan color="gray">최근 업데이트: 2시간 전</S.InfoSpan> */}
 				</div>
 			</S.UserInfo>
