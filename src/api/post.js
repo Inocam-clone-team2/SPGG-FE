ea56d0836a01937070bc27458a4505f9fff0f7ff
solightnsalt <>
2023-07-27 02:24:48 +0900

    Feat: 어딘가 고장난 조회와 삭제 구현

--- src/api/post.js
+++ src/api/post.js
@@ -50,21 +50,33 @@ export const addComment = async (payload) => {
 	return response.data;
 };
 
-// 댓글 삭제
-export const deleteComment = async (payload) => {
-	const postId = payload["postId"]; // 게시물 ID
-	const commentId = payload["commentId"]; // 댓글 ID
-	const response = await instance.delete(`/api/post/${postId}/comment/${commentId}`);
+// 댓글 조회
+export const fetchComment = async (payload) => {
+	const postId = payload.queryKey[1];
+	const response = await instance.get(`/api/post/${postId}/comment`);
 	return response.data;
 };
 
-// 댓글 수정
-export const modifyComment = async (payload) => {
-	const postId = payload["postId"];
-	const commentId = payload["commentId"];
-	const editComment = payload["editComment"];
-	const response = await instance.put(`/api/post/${postId}/comment/${commentId}`, editComment, {
-		headers: { "Content-Type": "application/json" },
-	});
-	return response.data;
+// 댓글 삭제
+export const deleteComment = async (payload) => {
+	try {
+		const postId = payload["id"]; // 게시물 ID
+		const commentId = payload["commentId"]; // 댓글 ID
+		const response = await instance.delete(`/api/post/${postId}/comment/${commentId}`);
+		return response.data;
+	} catch (error) {
+		if (error.response) {
+			// 서버 응답에 오류가 포함된 경우
+			console.error("댓글 삭제 오류:", error.response.data);
+			throw new Error("댓글 삭제에 실패했습니다. 잠시 후 다시 시도해주세요.");
+		} else if (error.request) {
+			// 요청을 보낸 후 응답을 받지 못한 경우 (네트워크 오류 등)
+			console.error("댓글 삭제 요청 오류:", error.request);
+			throw new Error("서버와의 연결이 끊어졌습니다. 잠시 후 다시 시도해주세요.");
+		} else {
+			// 기타 다른 오류가 발생한 경우
+			console.error("댓글 삭제 오류:", error.message);
+			throw new Error("댓글 삭제에 실패했습니다. 잠시 후 다시 시도해주세요.");
+		}
+	}
 };
--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -7,13 +7,17 @@ import { addComment, fetchComment, deleteComment, modifyComment } from "../../ap
 
 const Comment = ({ commentList }) => {
 	const { id } = useParams();
-	const navigate = useNavigate();
 	const queryClient = useQueryClient();
 	const [commentInput, setCommentInput] = useState("");
 
 	// 댓글 작성 핸들러
 	const handleCommentSubmit = async (e) => {
 		e.preventDefault();
+
+		if (!commentInput) {
+			alert("댓글 내용을 입력해주세요.");
+			return;
+		}
 		// 댓글 작성 요청
 		try {
 			await addComment({ id, newComment: { content: commentInput } });
@@ -23,22 +27,36 @@ const Comment = ({ commentList }) => {
 			// 댓글 작성 후 댓글 내용 초기화
 			setCommentInput("");
 		} catch (error) {
-			console.error("댓글 작성 오류:", error);
-			// 댓글 작성에 실패하면 오류 처리
 			alert("댓글 작성에 실패했습니다.");
+		}
+	};
+	// 댓글 조회 GET api/post/31/comment 403
+	const { data: test } = useQuery(["comments", id], fetchComment, {
+		staleTime: 300000,
+	});
+	// console.log("data", test);
+
+	// 댓글 삭제 핸들러
+	const handleCommentDelete = async (commentId) => {
+		// 댓글 삭제 요청
+		try {
+			await deleteComment({ id, commentId: commentId });
+			// 댓글 삭제가 성공적으로 완료되면 해당 쿼리를 재요청하여 업데이트
+			queryClient.invalidateQueries(["comments", postId]);
+		} catch (error) {
+			alert("잠시 후 다시 시도해주세요.");
 		}
 	};
 
 	//댓글 수
 	const commentCount = commentList?.length ?? 0;
-	console.log(commentCount, commentList);
-
+	console.log(commentList);
 	return (
 		<S.CommentWrapper>
 			<S.CommentHeader>
 				<h3>댓글</h3>
 				<S.CommentSpan>
-					총 <S.CommentSpan color="#16ae81">0</S.CommentSpan>개
+					총 <S.CommentSpan color="#16ae81">{commentCount}</S.CommentSpan>개
 				</S.CommentSpan>
 			</S.CommentHeader>
 			<S.CommentForm>
@@ -68,8 +86,12 @@ const Comment = ({ commentList }) => {
 									</S.CommentSpan>
 								</h4>
 								<S.CommentContent>{comment.content}</S.CommentContent>
-								<S.CommentBtn>수정</S.CommentBtn>
-								<S.CommentBtn color="#e84057">삭제</S.CommentBtn>
+								{/* <S.CommentBtn>수정</S.CommentBtn> */}
+								<S.CommentBtn
+									onClick={() => handleCommentDelete(comment.id)}
+									color="#e84057">
+									삭제
+								</S.CommentBtn>
 							</S.EachComment>
 						))}
 					</>
